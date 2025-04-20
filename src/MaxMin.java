57862f45b3644266712db08c93ab58a50dbdd393
Kateryna2882 <govoryhina82@gmail.com>
2025-04-20 19:11:01 +0300

    add MaxMin class

---
+++ src/MaxMin.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MaxMin {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int maximum;
+        int minimum;
+
+        //write your code here
+
+        System.out.print(maximum + " " + minimum);
+    }
+
+}

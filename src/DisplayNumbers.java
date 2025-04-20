42b07e70394df9dc85608fa8f87e163852ab6f0f
Kateryna2882 <govoryhina82@gmail.com>
2025-04-20 17:58:45 +0300

    add DisplayNumbers class

---
+++ src/DisplayNumbers.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class DisplayNumbers {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            numbers.add(Integer.parseInt(reader.readLine()));
+        }
+        //write your code here
+
+    }
+
+}

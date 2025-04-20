8299afc8d3bd39ae8fdb431419398ffb51544046
Kateryna2882 <govoryhina82@gmail.com>
2025-04-20 19:21:11 +0300

    add fields

--- src/MaxMin.java
+++ src/MaxMin.java
@@ -1,17 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 
 public class MaxMin {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        int maximum;
-        int minimum;
+        int[] numbers = new int[20];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = Integer.parseInt(reader.readLine());
+        }
 
-        //write your code here
+        int maximum = numbers[0];
+        int minimum = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > maximum) {
+                maximum = numbers[i];
+            }
 
+            if (numbers[i] < minimum) {
+                minimum = numbers[i];
+            }
+        }
         System.out.print(maximum + " " + minimum);
     }
-
 }

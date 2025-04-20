f65be336cafefb465f06ba6011d3fdee621f9071
Kateryna2882 <govoryhina82@gmail.com>
2025-04-20 18:47:48 +0300

    add ReplayProgram class

---
+++ src/ReplayProgram.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+//Наразі програма читає рядки з клавіатури, поки ви не введете порожній рядок.
+//Усі введені рядки перетворюються у верхній регістр і відображаються на екрані. Наприклад, якщо введено рядок «Мама» , а за ним порожній рядок, тоді має відображатися «МАМА»
+//. Модифікована програма повинна читати рядки з клавіатури, поки ви не введете порожній рядок.
+//Потім він повинен створити новий список рядків за таким алгоритмом:
+//
+//якщо вхідний рядок містить парну кількість символів, то рядок повторюється, розділяючись пробілом:
+//"qq" -> "qq qq"
+//"forty one" -> "forty one сорок один"
+//якщо непарне, то воно повторюється тричі:
+//"z" -> "zz z"
+//"j r" -> "jrjrj r"
+//". 1" -> ". 1 . 1 . 1"
+//"bug" -> "bug bug bug"
+//Програма відобразить вміст нового списку.
+
+public class ReplayProgram {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<String> strings = new ArrayList<String>();
+        while (true) {
+            String string = reader.readLine();
+            if (string == null || string.isEmpty()) break;
+            strings.add(string);
+        }
+
+        ArrayList<String> resultStrings = new ArrayList<String>();
+        for (String string : strings) {
+            resultStrings.add(string.toUpperCase());
+        }
+
+        for (String resultString : resultStrings) {
+            System.out.println(resultString);
+        }
+    }
+
+}

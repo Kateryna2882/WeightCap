09ecb014929d01b1404fa6fb59218f4aa92ba93a
Kateryna2882 <govoryhina82@gmail.com>
2025-04-21 15:03:36 +0300

    add fields

--- src/Order.java
+++ src/Order.java
@@ -19,9 +19,15 @@ public class Order {
     public static void sort(int[] array) {
         //write your code here
         for (int i = 0; i < array.length; i++) {
-
-
+            for (int j = i; j < array.length; j++) {
+                if (array[i] < array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
         }
-    }
 
+    }
 }
+

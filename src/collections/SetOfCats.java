feef288cf49597954313c577bbfdff26a7c699c4
Kateryna2882 <govoryhina82@gmail.com>
2025-04-27 14:45:53 +0300

    add SetOfCats class

---
+++ src/collections/SetOfCats.java
@@ -0,0 +1,28 @@
+package collections;
+import java.util.HashSet;
+import java.util.Set;
+public class SetOfCats {
+    public static void main(String[] args) {
+        Set<MapOfCats.Cat> cats = createCats();
+
+        //write your code here. step 3
+
+        printCats(cats);
+    }
+
+    public static Set<MapOfCats.Cat> createCats() {
+        //write your code here. step 2
+        Set<MapOfCats.Cat> cats = new HashSet<>();
+        for (int i = 0; i < 3; i++) {
+
+
+        }
+        return null;
+    }
+
+    public static void printCats(Set<MapOfCats.Cat> cats) {
+        // step 4
+    }
+
+    // step 1
+}

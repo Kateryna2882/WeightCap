2ec448c9e6e39dfb4f49a0d46242f80a1a6ff83d
Kateryna2882 <govoryhina82@gmail.com>
2025-04-27 12:30:33 +0300

    add fields

--- src/collections/GreaterThanTen.java
+++ src/collections/GreaterThanTen.java
@@ -1,30 +1,46 @@
 package collections;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class GreaterThanTen {
     public static Set<Integer> createSet() {
-        // write your code here
-        Set<Integer> set = new java.util.HashSet<>();
-        for (int i = 0; i < 20; i++) {
-            set.add((int) (Math.random() * 20));
+        Set<Integer> set = new HashSet<>();
+        set.add(16);
+        set.add(26);
+        set.add(36);
+        set.add(46);
+        set.add(56);
+        set.add(66);
+        set.add(76);
+        set.add(86);
+        set.add(10);
+        set.add(6);
 
-        }
+        set.add(17);
+        set.add(27);
+        set.add(37);
+        set.add(47);
+        set.add(57);
+        set.add(67);
+        set.add(77);
+        set.add(87);
+        set.add(97);
+        set.add(7);
+
         return set;
     }
 
     public static void removeAllNumbersGreaterThan10(Set<Integer> set) {
-        // write your code here
-        for (Integer i : set) {
-            if (i > 10) {
-                set.remove(i);
+        Set<Integer> copy = new HashSet<>(set);
+        for (Integer number : copy) {
+            if (number > 10) {
+                set.remove(number);
             }
         }
-
     }
 
     public static void main(String[] args) {
 
     }
-
 }

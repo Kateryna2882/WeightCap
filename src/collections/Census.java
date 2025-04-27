259f7e1cd2d6b4fced75d333c2c21464c913a1fb
Kateryna2882 <govoryhina82@gmail.com>
2025-04-27 12:40:59 +0300

    add fields

--- src/collections/Census.java
+++ src/collections/Census.java
@@ -5,55 +5,42 @@ import java.util.Map;
 
 public class Census {
     public static Map<String, String> createMap() {
-        //write your code here
         Map<String, String> map = new HashMap<>();
-        map.put("Sim", "Sim");
-        map.put("Tom", "Tom");
-        map.put("Arbus", "Arbus");
-        map.put("Baby", "Baby");
-        map.put("Cat", "Cat");
-        map.put("Dog", "Dog");
-        map.put("Eat", "Eat");
-        map.put("Food", "Food");
-        map.put("Gevey", "Gevey");
+        map.put("Smith", "Sophia");
+        map.put("Johnson", "Isabella");
+        map.put("Williams", "Jacob");
+        map.put("Brown", "Mason");
+        map.put("Jones", "William");
+        map.put("Garcia", "Jayden");
+        map.put("Miller", "Emily");
+        map.put("Davis", "Abigail");
+        map.put("Rodriguez", "Mia");
+        map.put("Martinez", "Joshua");
         return map;
-
     }
 
     public static int getSameFirstNameCount(Map<String, String> map, String name) {
-        //write your code here
-        Map<String, Integer> map2 = new HashMap<>();
-        for (Map.Entry<String, String> pair : map.entrySet()) {
-            if (pair.getValue().equals(name)) {
-                if (map2.containsKey(pair.getKey())) {
-                    map2.put(pair.getKey(), map2.get(pair.getKey()) + 1);
-                } else {
-                    map2.put(pair.getKey(), 1);
-                }
+        int count = 0;
+        for (String nameTmp : map.values()) {
+            if (nameTmp.equals(name)) {
+                count++;
             }
         }
-        return map2.size();
+        return count;
     }
 
     public static int getSameLastNameCount(Map<String, String> map, String lastName) {
-        //write your code here
-        Map<String, Integer> map2 = new HashMap<>();
-        for (Map.Entry<String, String> pair : map.entrySet()) {
-            if (pair.getKey().equals(lastName)) {
-                if (map2.containsKey(pair.getValue())) {
-                    map2.put(pair.getValue(), map2.get(pair.getValue()) + 1);
-                } else {
-                    map2.put(pair.getValue(), 1);
-                }
+        int count = 0;
+        for (String lastNameTmp : map.keySet()) {
+            if (lastNameTmp.equals(lastName)) {
+                count++;
             }
         }
-        return map2.size();
-
+        return count;
 
     }
 
     public static void main(String[] args) {
 
     }
-
 }

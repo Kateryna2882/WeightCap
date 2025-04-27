2287877e2eee9547aa6f83fe6506a5ee91f55032
Kateryna2882 <govoryhina82@gmail.com>
2025-04-27 17:23:50 +0300

    add SharedLast class

---
+++ src/collections/SharedLast.java
@@ -0,0 +1,23 @@
+package collections;
+
+import java.util.Map;
+
+public class SharedLast {
+    public static void main(String[] args) {
+        Map<String, String> map = createPeopleMap();
+        printPeopleMap(map);
+    }
+
+    public static Map<String, String> createPeopleMap() {
+        //write your code here
+
+        return null;
+    }
+
+    public static void printPeopleMap(Map<String, String> map) {
+        for (Map.Entry<String, String> s : map.entrySet()) {
+            System.out.println(s.getKey() + " " + s.getValue());
+        }
+    }
+
+}

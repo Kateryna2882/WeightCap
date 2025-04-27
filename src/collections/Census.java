4ae2ab6472bdb1a01c528db94d447e6f20fd3c8f
Kateryna2882 <govoryhina82@gmail.com>
2025-04-27 12:32:21 +0300

    add Census class

---
+++ src/collections/Census.java
@@ -0,0 +1,25 @@
+package collections;
+
+import java.util.Map;
+
+public class Census {
+    public static Map<String, String> createMap() {
+        //write your code here
+
+    }
+
+    public static int getSameFirstNameCount(Map<String, String> map, String name) {
+        //write your code here
+
+    }
+
+    public static int getSameLastNameCount(Map<String, String> map, String lastName) {
+        //write your code here
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}

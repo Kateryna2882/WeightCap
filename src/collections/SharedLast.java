8f6e63c012db5bdd0a8db788723b64d304715d57
Kateryna2882 <govoryhina82@gmail.com>
2025-04-27 17:30:45 +0300

    add fields

--- src/collections/SharedLast.java
+++ src/collections/SharedLast.java
@@ -1,5 +1,6 @@
 package collections;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class SharedLast {
@@ -10,8 +11,19 @@ public class SharedLast {
 
     public static Map<String, String> createPeopleMap() {
         //write your code here
+        Map<String, String> map = new HashMap<>();
+        map.put("Smith", "John");
+        map.put("Johnson", "Richard");
+        map.put("Williams", "Robert");
+        map.put("Brown", "Michael");
+        map.put("Jones", "Richard");
+        map.put("Garcia", "Donald");
+        map.put("Miller", "Nathan");
+        map.put("Dav", "Matt");
+        map.put("Davis", "Matthew");
+        map.put("Davis", "Matew");
 
-        return null;
+        return map;
     }
 
     public static void printPeopleMap(Map<String, String> map) {

c517a7cf68bce0bc8017546b020cedd76a54eb9a
Kateryna2882 <govoryhina82@gmail.com>
2025-04-27 12:47:05 +0300

    add fields

--- src/collections/FirstLastName.java
+++ src/collections/FirstLastName.java
@@ -6,11 +6,26 @@ import java.util.Map;
 public class FirstLastName {
     public static Map<String, String> createMap() {
         //write your code here
-
+        Map<String, String> map = new HashMap<>();
+        map.put("Sim", "Sim");
+        map.put("Tom", "Tom");
+        map.put("Arbus", "Arbus");
+        map.put("Baby", "Baby");
+        map.put("Cat", "Cat");
+        map.put("Dog", "Dog");
+        map.put("Eat", "Eat");
+        map.put("Food", "Food");
+        return map;
     }
 
     public static void removeFirstNameDuplicates(Map<String, String> map) {
         //write your code here
+        Map<String, String> copy = new HashMap<>(map);
+        for (Map.Entry<String, String> pair : copy.entrySet()) {
+            if (pair.getKey().equals(pair.getValue())) {
+                map.remove(pair.getKey());
+            }
+        }
 
     }
 

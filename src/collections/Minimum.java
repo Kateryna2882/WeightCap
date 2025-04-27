c8dd2f25a0fdf21260f9becbc12c927a7e2531f7
Kateryna2882 <govoryhina82@gmail.com>
2025-04-27 18:01:08 +0300

    add Minimum class

---
+++ src/collections/Minimum.java
@@ -0,0 +1,25 @@
+package collections;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.List;
+
+public class Minimum {
+    public static void main(String[] args) throws Exception {
+        List<Integer> integerList = getIntegerList();
+        System.out.println(getMinimum(integerList));
+    }
+
+    public static int getMinimum(List<Integer> list) {
+        // Find the minimum here
+        return 0;
+    }
+
+    public static List<Integer> getIntegerList() throws IOException {
+        // Create and initialize a list here
+        BufferedReader reader = new BufferedReader(new java.io.InputStreamReader(System.in));
+
+        return null;
+    }
+
+}

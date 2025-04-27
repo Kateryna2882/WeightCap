5c8f1a2ecd87e7b79d4156b9d80c03b0acdb11ac
Kateryna2882 <govoryhina82@gmail.com>
2025-04-27 12:17:26 +0300

    add fields

--- src/collections/Words.java
+++ src/collections/Words.java
@@ -5,17 +5,31 @@ import java.util.Set;
 
 public class Words {
     public static Set<String> createSet() {
-        //write your code here
-        HashSet<String> set = new HashSet<>();
-        for (int i = 0; i < 20; i++) {
-            set.add(String.valueOf(i));
-        }
+        Set<String> set = new HashSet<>();
+        set.add("London");
+        set.add("Label");
+        set.add("Lack");
+        set.add("Landing");
+        set.add("Lane");
+        set.add("Last");
+        set.add("Line");
+        set.add("Left");
+        set.add("Laugh");
+        set.add("Lately");
+        set.add("Later");
+        set.add("Large");
+        set.add("Landlord");
+        set.add("Landfill");
+        set.add("Ladder");
+        set.add("Lady");
+        set.add("Language");
+        set.add("Lecture");
+        set.add("Leave");
+        set.add("Learn");
         return set;
-
     }
 
     public static void main(String[] args) {
 
     }
-
 }

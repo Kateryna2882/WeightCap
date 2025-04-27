cb1fb6945ff3f22a8ae6556e3bdf68ecaa7ae0cf
Kateryna2882 <govoryhina82@gmail.com>
2025-04-27 11:24:40 +0300

    add SolutionALLL class and fields

---
+++ src/collections/SolutionALLL.java
@@ -0,0 +1,24 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class SolutionALLL {
+    public static Object createArrayList() {
+        //write your code here
+        ArrayList<String> list = new ArrayList<>();
+        return list;
+    }
+
+    public static Object createLinkedList() {
+        //write your code here
+        LinkedList<String> list = new LinkedList<>();
+        return list;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
